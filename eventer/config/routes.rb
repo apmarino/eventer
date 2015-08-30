3e44eef5f6c54470d13318c481f3d17fb1b067be
Alexander Marino <marinoalexp@gmail.com>
2015-08-30 14:34:52 -0400

    welcome control/index and root

--- eventer/config/routes.rb
+++ eventer/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  resource :sessions, only: [:create, :destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
