94bd9b694cd92c3a292681141caa6b9d84d92b78
Benjamin Aplin <benjaminaplin@gmail.com>
2015-08-31 10:00:02 -0400

    add paperclip

--- eventer/app/controllers/users_controller.rb
+++ eventer/app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+	class UsersController < ApplicationController
 	def index
 		@users =User.all
 	end
@@ -9,13 +9,14 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.create(user_params)
-		if @user.save
-			session[:user_id] = @user.id
-		 redirect_to users_path
-		else
-			flash[:error] = "You fucked up something"
-			redirect_to '/'
-		end
+		redirect_to users_path
+		# if @user.save
+		# 	session[:user_id] = @user.id
+		#  redirect_to users_path
+		# else
+		# 	flash[:error] = "You fucked up something"
+		# 	redirect_to '/'
+		# end
 	end
 
 	def show
--- eventer/db/schema.rb
+++ eventer/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150829195825) do
+ActiveRecord::Schema.define(version: 20150830232554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,8 +40,12 @@ ActiveRecord::Schema.define(version: 20150829195825) do
     t.text     "about_me"
     t.string   "avatar"
     t.string   "twitter"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
