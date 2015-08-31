939646a0153063f3e1d70bf6abc0ecbbee6fd712
Alexander Marino <marinoalexp@gmail.com>
2015-08-31 10:41:54 -0400

    logout and some redirects in there

--- eventer/app/controllers/sessions_controller.rb
+++ eventer/app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by({user_name: params[:user_name]})  #unique username is better
+    binding.pry
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to users_path
@@ -9,5 +10,10 @@ class SessionsController < ApplicationController
       flash[:error] = "Incorrect user Name or Password"
       redirect_to '/'
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
   end
 end
