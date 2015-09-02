2bc7ab984e30167f81becb57c85efaf9420030bb
Alexander Marino <marinoalexp@gmail.com>
2015-09-02 10:06:47 -0400

    latlong added migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901170547) do
+ActiveRecord::Schema.define(version: 20150902135748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20150901170547) do
     t.datetime "date"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "lat"
+    t.string   "long"
   end
 
   create_table "events_users", force: :cascade do |t|
